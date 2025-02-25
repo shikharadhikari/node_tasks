65f91292f1780cad663d2962b5655e96c37424b9
Shikhar Adhikari <67640904+shikharadhikari@users.noreply.github.com>
2025-02-25 19:26:13 +0530

    Add files via upload

--- 1/3.js
+++ 1/3.js
@@ -1,9 +1,9 @@
-function createCode(name, dob){
+function createCode(name, dob) {
   let ini = name.slice(0, 3);
   ini = ini.toUpperCase();
   let year = dob.slice(2, 4);
   let combined = ini + year;
   return combined;
 }
-const code = createCode("Aryan", "2015-06-27");
+const code = createCode("John", "1995-06-15");
 console.log(code);
